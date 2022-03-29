700ef7bfe2f36ca6682a6d71a64f79d54a72a3db
Sander Hageman <sander@shageman.nl>
2022-03-29 23:17:29 +0200

    day 16 p1

--- src/day16.rs
+++ src/day16.rs
@@ -1,18 +1,29 @@
 use hex::FromHex;
 use nom::bits::complete::{tag, take};
 use nom::branch::alt;
-use nom::character::complete::newline;
+use nom::multi::count;
 use nom::multi::many0;
-use nom::multi::many1;
-use nom::multi::many_m_n;
 use nom::sequence::pair;
 use nom::sequence::preceded;
-use nom::sequence::terminated;
-use nom::sequence::tuple;
-use nom::{combinator::*, multi::separated_list1, IResult};
+use nom::IResult;
 
-type TParsed = Vec<TParsedSub>;
-type TParsedSub = Vec<usize>;
+type THeader = (usize, usize);
+#[derive(Debug)]
+enum Packet {
+  Lit(THeader, usize),
+  Op(THeader, Vec<Packet>),
+}
+
+impl Packet {
+  fn get_v_sum(&self) -> usize {
+    match self {
+      Packet::Lit((v, _), _) => *v,
+      Packet::Op((v, _), sub) => v + sub.iter().map(|p| p.get_v_sum()).sum::<usize>(),
+    }
+  }
+}
+
+type TParsed = Packet;
 
 type PType<'a> = (&'a [u8], usize);
 
@@ -24,7 +35,7 @@ pub fn day<'a>(input: String) -> (usize, usize) {
 }
 
 fn part_1(input: &TParsed) -> usize {
-  0
+  input.get_v_sum()
 }
 
 fn part_2(input: &TParsed) -> usize {
@@ -32,29 +43,23 @@ fn part_2(input: &TParsed) -> usize {
 }
 
 fn parse<'a>(input: &'a str) -> TParsed {
-  match try_parse((input.as_bytes(), 0)) {
+  match try_parse((&Vec::from_hex(input).expect("non"), 0)) {
     Ok((_, v)) => v,
     Err(e) => panic!("error parsing: {:?}", e),
   }
 }
 
-fn try_parse<'a>(i: PType<'a>) -> IResult<PType<'a>, TParsed> {
-  // all_consuming(separated_list1(newline, parse_y))(i);
-  unimplemented!()
+fn try_parse<'a>(i: PType<'a>) -> IResult<PType<'a>, Packet> {
+  parse_packet(i)
 }
 
-fn parse_y<'a>(i: PType<'a>) -> IResult<PType<'a>, TParsedSub> {
-  // tuple((take(3usize), take(3usize)))(i);
-
-  unimplemented!()
-}
-
-fn parse_packet<'a>(i: PType<'a>) -> IResult<PType<'a>, (u8, usize)> {
+fn parse_packet<'a>(i: PType<'a>) -> IResult<PType<'a>, Packet> {
   alt((parse_lit, parse_op))(i)
 }
 
-fn parse_lit<'a>(i: PType<'a>) -> IResult<PType<'a>, (u8, usize)> {
-  let (i, v) = terminated(take(3usize), tag(4, 3usize))(i)?;
+fn parse_lit<'a>(i: PType<'a>) -> IResult<PType<'a>, Packet> {
+  let (i, v) = take(3usize)(i)?;
+  let (i, _) = tag(4, 3usize)(i)?;
 
   let p_head = preceded(tag(1, 1usize), take(4usize));
   let p_tail = preceded(tag(0, 1usize), take(4usize));
@@ -68,29 +73,37 @@ fn parse_lit<'a>(i: PType<'a>) -> IResult<PType<'a>, (u8, usize)> {
   t <<= 4;
   t |= r;
 
-  Ok((i, (v, t)))
+  Ok((i, Packet::Lit((v, 4), t)))
 }
 
-fn parse_op<'a>(i: PType<'a>) -> IResult<PType<'a>, (u8, usize)> {
+fn parse_op<'a>(i: PType<'a>) -> IResult<PType<'a>, Packet> {
   let (i, v) = take(3usize)(i)?;
   let (i, t) = take(3usize)(i)?;
-
-  let a = alt((parse_op_0, parse_op_1))(i)?;
-  println!("{:?}", a);
-
-  Ok((i, (v, t)))
+  let (i, sub_packets) = alt((parse_op_0, parse_op_1))(i)?;
+  Ok((i, Packet::Op((v, t), sub_packets)))
 }
 
-fn parse_op_0<'a>(i: PType<'a>) -> IResult<PType<'a>, Vec<(u8, usize)>> {
+fn parse_op_0<'a>(i: PType<'a>) -> IResult<PType<'a>, Vec<Packet>> {
   let (i, _) = tag(0, 1usize)(i)?;
-  let (i, _): (_, usize) = take(15usize)(i)?;
-  many0(parse_packet)(i) // TODO check length
+  let (i, l): (_, usize) = take(15usize)(i)?;
+
+  let take_bite = take(8usize);
+  let (i, mut new_bytes) = count(take_bite, l / 8)(i)?;
+  let rest = l % 8;
+
+  let (i, yeet): (_, u8) = take(rest)(i)?;
+
+  new_bytes.push(yeet << (8 - rest));
+
+  let (_, p) = many0(parse_packet)((&new_bytes, 0)).expect("unable to parse subpackets");
+
+  Ok((i, p))
 }
 
-fn parse_op_1<'a>(i: PType<'a>) -> IResult<PType<'a>, Vec<(u8, usize)>> {
+fn parse_op_1<'a>(i: PType<'a>) -> IResult<PType<'a>, Vec<Packet>> {
   let (i, _) = tag(1, 1usize)(i)?;
   let (i, l): (_, usize) = take(11usize)(i)?;
-  many_m_n(l, l, parse_packet)(i)
+  count(parse_packet, l)(i)
 }
 
 #[test]
@@ -105,6 +118,12 @@ fn yee_op_0() {
   let a = Vec::from_hex("38006F45291200").expect("yeet");
   let input = parse_packet((&a, 0));
   println!("Out: {:?}", input);
+}
+
+#[test]
+fn show_parse_16() {
+  let input = parse(EXAMPLE_INPUT[0].0);
+  println!("{:?}", input);
 }
 
 #[test]
